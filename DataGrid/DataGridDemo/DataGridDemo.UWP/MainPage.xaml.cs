f961db3d0325f58d7aeb2ccc7af4a3f6f0950648
Karthik Raja <Karthikraja.kalaimani@syncfusion.com>
2020-10-27 17:44:52 +0530

    How to change the default theme color of Xamarin Forms SfDataGrid

---
+++ DataGrid/DataGridDemo/DataGridDemo.UWP/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Syncfusion.XForms.Themes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace DataGridDemo.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Syncfusion.SfDataGrid.XForms.UWP.SfDataGridRenderer.Init();
+            ThemeSelectorService.SetRequestedThemeAsync(ElementTheme.Dark);
+            LoadApplication(new DataGridDemo.App());
+        }
+    }
+
+    public static class ThemeSelectorService
+    {
+
+        public static async Task SetRequestedThemeAsync(ElementTheme Theme)
+        {
+            foreach (var view in CoreApplication.Views)
+            {
+                await view.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    if (Window.Current.Content is FrameworkElement frameworkElement)
+                    {
+                        frameworkElement.RequestedTheme = Theme;
+
+                        ICollection<Xamarin.Forms.ResourceDictionary> mergedDictionaries = Xamarin.Forms.Application.Current.Resources.MergedDictionaries;
+                        if (Theme == ElementTheme.Dark)
+                        {
+                            mergedDictionaries.Clear();
+                            mergedDictionaries.Add(new DarkTheme());
+                        }
+                        else if (Theme == ElementTheme.Light)
+                        {
+                            mergedDictionaries.Clear();
+                            mergedDictionaries.Add(new LightTheme());
+                        }
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ DataGrid/DataGridDemo/DataGridDemo/ViewModel/ViewModel.cs
@@ -0,0 +1,85 @@
+﻿using Syncfusion.Data.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DataGridDemo
+{
+    public class ViewModel : NotificationObject
+    {
+        internal OrderInfoRepository order;
+
+        internal string[] Customers = new string[] { "Adams", "Crowley", "Ellis", "Gable", "Irvine", "Keefe", "Mendoza", "Owens", "Rooney", "Wadded", };
+
+        public ViewModel()
+        {
+            order = new OrderInfoRepository();
+            SetRowstoGenerate(100);
+            SelectedItem = this.ordersInfo[2];
+            this.CustomerNames = Customers.ToObservableCollection();
+        }
+
+        #region ItemsSource
+
+        public ObservableCollection<string> CustomerNames { get; set; }
+
+        private ObservableCollection<OrderInfo> ordersInfo;
+        public ObservableCollection<OrderInfo> OrdersInfo
+        {
+            get { return ordersInfo; }
+            set
+            {
+                this.ordersInfo = value;
+                RaiseCollectionChanged("OrdersInfo");
+            }
+        }
+
+        private OrderInfo selectedItem;
+
+        public OrderInfo SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                this.selectedItem = value;
+                RaiseCollectionChanged("SelectedItem");
+            }
+        }
+
+        #endregion
+
+        #region ItemSource Generator
+
+        public void SetRowstoGenerate(int count)
+        {
+            OrdersInfo = order.GetOrderDetails(count);
+        }
+
+        #endregion
+    }
+
+    public class NotificationObject : INotifyPropertyChanged, INotifyCollectionChanged
+    {
+        public void RaisePropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaiseCollectionChanged(string propName)
+        {
+            if (this.CollectionChanged != null)
+                this.CollectionChanged(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+    }
+}
